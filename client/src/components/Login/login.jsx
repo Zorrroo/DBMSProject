e65c9665f13b0d4963045bc74ae52a004b9c8006
Bellana Yaswanth <yaswanth1847@gmail.com>
2024-03-25 16:44:47 +0530

    port 4000

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch('http://localhost:3000/login', {
+            const response = await fetch('http://localhost:4000/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const db = new sqlite3.Database('./database.db', (err) => {
   if (err) {
