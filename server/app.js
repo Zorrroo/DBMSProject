89d6fdca9205be49395904769e1128089e8bbafd
Bellana Yaswanth <yaswanth1847@gmail.com>
2024-04-07 12:28:20 +0530

    vs11

--- server/app.js
+++ server/app.js
@@ -90,6 +90,18 @@ app.get("/:transport", async (request, response) => {
   }
 });
 
+// app.get("/lists", async (request, response) => {
+//   const query1 = `SELECT departure, destination FROM trains;`;
+//   db.all(query1, (error, results) => {
+//     if (error) {
+//       console.error("Error fetching data:", error);
+//       return response.status(500).json({ error: "Internal Server Error" });
+//     }
+//     response.json(results);
+
+//   });
+// });
+
 app.post("/login", async (request, response) => {
   const { name, email, password } = request.body;
   if (!name || !email || !password) {
